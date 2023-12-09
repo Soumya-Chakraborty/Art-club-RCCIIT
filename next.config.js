b2f7e9e24ca099f9a547680c2008407da3a517ed
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2023-12-10 00:18:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  target: 'experimental-serverless-trace', // Ensure this is set for static export
+  // Remove the deprecated 'target' property
+  // target: 'experimental-serverless-trace',
+
+  // Correct the experimental options
+  experimental: {
+    // Remove the 'images' property if you are not using it
+    // images: true,
+
+    // Add other experimental options if needed
+  },
+
   // Add other configurations as needed
 }
 
