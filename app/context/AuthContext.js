ce71bd0dda6721d51308a1f2266c2af5b6de468e
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2023-12-10 10:15:12 +0530

    Update AuthContext.js

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -1,5 +1,5 @@
-// Mark AuthContext as a client component
 "use client";
+// Importing necessary modules from React and Firebase
 import { useContext, createContext, useState, useEffect } from "react";
 import {
   signInWithPopup,
@@ -9,27 +9,38 @@ import {
 } from "firebase/auth";
 import { auth } from "../firebase";
 
+// Creating a context for authentication
 const AuthContext = createContext();
 
+// Creating a provider component to manage authentication state
 export const AuthContextProvider = ({ children }) => {
+  // State to store the current user
   const [user, setUser] = useState(null);
 
+  // Function to handle Google sign-in
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
 
+  // Function to handle log out
   const logOut = () => {
     signOut(auth);
   };
 
+  // Effect to listen for changes in authentication state
   useEffect(() => {
+    // Subscribe to the auth state changes
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // Update the user state when the authentication state changes
       setUser(currentUser);
     });
-    return () => unsubscribe();
-  }, [user]);
 
+    // Unsubscribe when the component unmounts
+    return () => unsubscribe();
+  }, [user]); // Dependency array includes user to react to changes in user state
+
+  // Providing the authentication context to the component tree
   return (
     <AuthContext.Provider value={{ user, googleSignIn, logOut }}>
       {children}
@@ -37,6 +48,7 @@ export const AuthContextProvider = ({ children }) => {
   );
 };
 
+// Custom hook to access the authentication context in components
 export const UserAuth = () => {
   return useContext(AuthContext);
 };
