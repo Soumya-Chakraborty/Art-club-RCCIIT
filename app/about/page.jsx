7d87678240b9e2d829def3a93fb3c1ec78acf379
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2023-12-10 09:56:58 +0530

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,43 +1,68 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { UserAuth } from "../context/AuthContext";
-import Spinner from "../components/Spinner";
+import React from 'react'
 
 const page = () => {
-  const { user } = UserAuth();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 50));
-      setLoading(false);
-    };
-    checkAuthentication();
-  }, [user]);
-
   return (
-    <div className="p-4">
-      {loading ? (
-        <Spinner />
-      ) : user ? (
-        <p className="text-2xl mb-4 text-white-600">
-            Hey <span className="text-red-500 font-bold">{user.displayName}</span>! 🎨 Welcome to the excitement of the <span className="italic">Animae Art Contest 2023</span>! 🚀 Your registration is confirmed, and we're thrilled to have you on board.
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">
+        Attention, creative minds and art enthusiasts!
+      </h1>
 
-Get ready to dive into a world of creativity and imagination. 🌟 As a contestant, you have the power to bring your unique vision to life. Whether it's vibrant characters, mesmerizing scenes, or innovative concepts, the canvas is yours.
+      <h2 className="text-2xl font-bold mb-4">The RCCIIT Art Club is thrilled to announce the much-anticipated Animae Art Contest 2023.</h2>
 
-Stay tuned for updates, announcements, and inspiration to fuel your artistic journey. Feel free to explore the contest community, connect with fellow artists, and share your progress. 🤝 Together, let's make this contest a celebration of artistry and talent.
+      <p className="text-lg">
+        <strong className="text-blue-200">Theme:</strong> Unleash the Power of Imagination in the World of Animae
+      </p>
 
-Best of luck, and may your artistic endeavors shine bright in the Animae Art Contest 2023! 🌈✨
+      <p className="text-lg mt-4">
+        Calling all artists, both budding and seasoned, to bring their wildest imaginings to life through the captivating medium of Animae. This is your chance to showcase your artistic prowess, delve into the realm of fantasy, and let your creativity soar!
+      </p>
 
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Key Details:</h2>
+        <p>
+          <strong className="text-blue-200">Contest Deadline:</strong> December 1, 2023
         </p>
-
-      ) : (
-        <p className="text-lg text-gray-700">
-          You must be logged in to view this page - protected route.
+        <p>
+          <strong className="text-blue-100">Category:</strong> Individual Submissions
         </p>
-      )}
-    </div>
-  );  
-};
+        <p>
+          <strong className="text-blue-100">Submission Guidelines:</strong> Please submit your entries to this form by the specified deadline. Include your name, contact information, and a brief description of your artwork (as asked within the form).
+        </p>
+      </div>
 
-export default page;
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Prizes Await:</h2>
+        <p>
+          The top entries will be awarded not only with recognition but also with exciting prizes. This is an excellent opportunity to gain exposure, connect with fellow artists, and celebrate the diverse talent within our community.
+        </p>
+      </div>
+
+      <p className="mt-8">
+        <strong className="text-blue-100">Get Ready to Dazzle Us with Your Creativity:</strong> Let your imagination run wild, and create art that transcends boundaries. The Animae Art Contest 2023 is not just a competition; it's a celebration of artistic expression and the magic of Animae.
+      </p>
+
+      <p className="mt-8">
+  For any inquiries, reach out to Soumya Chakraborty of ITA 2nd Year at{' '}
+  <a href="mailto:soumyachakraborty198181@gmail.com" className="text-blue-500 hover:underline">
+    soumyachakraborty198181@gmail.com
+  </a>
+  {' '} or at{' '}
+  <a href="tel:+918910927312" className="text-blue-500 hover:underline">
+    +918910927312
+  </a>.
+</p>
+
+
+
+      <p className="mt-8 text-lg font-bold">
+        We can't wait to witness the magic you'll bring to life!
+      </p>
+
+      <p className="mt-8 text-2xl font-bold text-blue-100">
+        RCCIIT Art Club
+      </p>
+    </main>
+  );
+}
+
+export default page
